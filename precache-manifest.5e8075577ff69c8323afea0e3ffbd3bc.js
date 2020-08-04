51525357fc4d40304fa620db83ab9b42495e7a09
Prosper Nnadi <Prospernnadi3@gmail.com>
2020-08-04 17:33:59 +0100

    Updates

---
+++ precache-manifest.5e8075577ff69c8323afea0e3ffbd3bc.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8b99688724a1b6a85f9274204df5b6",
+    "url": "/PetsittingApp/index.html"
+  },
+  {
+    "revision": "d3d492b2ae4edac64d0b",
+    "url": "/PetsittingApp/static/css/2.da8c7b4f.chunk.css"
+  },
+  {
+    "revision": "08a147653362a6d36a23",
+    "url": "/PetsittingApp/static/css/main.be971a7a.chunk.css"
+  },
+  {
+    "revision": "d3d492b2ae4edac64d0b",
+    "url": "/PetsittingApp/static/js/2.dec505a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PetsittingApp/static/js/2.dec505a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08a147653362a6d36a23",
+    "url": "/PetsittingApp/static/js/main.5be7ea99.chunk.js"
+  },
+  {
+    "revision": "7c5e7a497372d910467a",
+    "url": "/PetsittingApp/static/js/runtime-main.9830ae68.js"
+  },
+  {
+    "revision": "5958d8063d9c539a32719e05a1961fc2",
+    "url": "/PetsittingApp/static/media/about-1.5958d806.jpg"
+  },
+  {
+    "revision": "597a629182db76feb146dabf13ef3aa0",
+    "url": "/PetsittingApp/static/media/bg_1.597a6291.jpg"
+  },
+  {
+    "revision": "079d49b8ab5fc4bbc5d4425f98c97fc5",
+    "url": "/PetsittingApp/static/media/bg_2.079d49b8.jpg"
+  },
+  {
+    "revision": "d9d09fb437dc31c1635302a0b00c0b5c",
+    "url": "/PetsittingApp/static/media/bg_3.d9d09fb4.jpg"
+  },
+  {
+    "revision": "e509eac7f0d9ad926df64ed86fb0ebd3",
+    "url": "/PetsittingApp/static/media/faq-img.e509eac7.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/PetsittingApp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/PetsittingApp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/PetsittingApp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/PetsittingApp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/PetsittingApp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "99fcb86b192980f5d2c09e70e712f709",
+    "url": "/PetsittingApp/static/media/gallery-1.99fcb86b.jpg"
+  },
+  {
+    "revision": "42794abb1370ddf44264ed50b2964cdd",
+    "url": "/PetsittingApp/static/media/gallery-2.42794abb.jpg"
+  },
+  {
+    "revision": "b0e6a7d19ca6452953536059f83b0f3a",
+    "url": "/PetsittingApp/static/media/gallery-3.b0e6a7d1.jpg"
+  },
+  {
+    "revision": "fcbf9cd6ee3f5ca58f51a58d5b490b95",
+    "url": "/PetsittingApp/static/media/gallery-4.fcbf9cd6.jpg"
+  },
+  {
+    "revision": "a01d6088e3874383ed95996d28545310",
+    "url": "/PetsittingApp/static/media/gallery-5.a01d6088.jpg"
+  },
+  {
+    "revision": "fcac38bb61788396c2ae09c454eaa3f4",
+    "url": "/PetsittingApp/static/media/gallery-6.fcac38bb.jpg"
+  },
+  {
+    "revision": "9567bcc389911fd6f94118a52506b595",
+    "url": "/PetsittingApp/static/media/image_1.9567bcc3.jpg"
+  },
+  {
+    "revision": "c494b8ffd394058738d4f0d5f8984f49",
+    "url": "/PetsittingApp/static/media/image_2.c494b8ff.jpg"
+  },
+  {
+    "revision": "89fdf5ffc7396bfd2425d395adde8a6f",
+    "url": "/PetsittingApp/static/media/image_3.89fdf5ff.jpg"
+  },
+  {
+    "revision": "bbcd5413ffcc46f7d9eef5fb36340659",
+    "url": "/PetsittingApp/static/media/person_1.bbcd5413.jpg"
+  },
+  {
+    "revision": "2cd3dcd36bb7c61c847ac770c2aff481",
+    "url": "/PetsittingApp/static/media/person_2.2cd3dcd3.jpg"
+  },
+  {
+    "revision": "514eee1b06ddf4a5692f7264d3da79aa",
+    "url": "/PetsittingApp/static/media/person_3.514eee1b.jpg"
+  },
+  {
+    "revision": "28d9178ca84b2a5e4b0c6f57a5a34a35",
+    "url": "/PetsittingApp/static/media/pricing-1.28d9178c.jpg"
+  },
+  {
+    "revision": "7a8f1d5294f051b8d8c877669055df39",
+    "url": "/PetsittingApp/static/media/pricing-2.7a8f1d52.jpg"
+  },
+  {
+    "revision": "2bed7f66684bc614f3212126d57e05a5",
+    "url": "/PetsittingApp/static/media/pricing-3.2bed7f66.jpg"
+  }
+]);
